5d1042ccb8c90efd16e6bc34677be0adf64265bf
gongle <skygongle@gmail.com>
2013-09-11 15:42:46 +0800

    框架整体搭建完工.

---
+++ modules/runner/src/main/java/org/hope/web/SiteMeshFilterExtend.java
@@ -0,0 +1,30 @@
+package org.hope.web;
+
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+import org.hope.utils.Strings;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * 扩充SiteMeshFilter Filter,增加跳过filter参数标识
+ *
+ * @see #IGNORE_DECORATE_FLAG
+ */
+public class SiteMeshFilterExtend extends SiteMeshFilter {
+    /**
+     * 忽略  SiteMeshFilter 参数标识*
+     */
+    public static String IGNORE_DECORATE_FLAG = "_ignore_decorate";
+
+    @Override
+    public void doFilter(ServletRequest rq, ServletResponse rs, FilterChain chain) throws IOException, ServletException {
+        if (Strings.isNotBlank(rq.getParameter(IGNORE_DECORATE_FLAG))) {
+            chain.doFilter(rq, rs);
+            return;
+        }
+        super.doFilter(rq, rs, chain);
+    }
+
+
+}
